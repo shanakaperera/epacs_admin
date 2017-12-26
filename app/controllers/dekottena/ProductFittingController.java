73b798e13cf85d204b8ac61f640a2a7bd5b9e2d0
sumedha <ssrimal@ymail.com>
2017-12-26 14:50:47 +0530

    making_fittingPage

--- app/controllers/dekottena/ProductFittingController.java
+++ app/controllers/dekottena/ProductFittingController.java
@@ -2,10 +2,11 @@ package controllers.dekottena;
 
 import play.mvc.*;
 
-public class ProductCoatingController extends Controller {
+public class ProductFittingController extends Controller {
 
     public Result home() {
-        return ok(views.html.dekottena.pr_coating_page.pr_coating.render());
+//        return ok(views.html.dekottena.pr_coating_page.pr_coating.render());
+        return ok(views.html.dekottena.pr_fitting_page.pr_fitting.render());
     }
 
     public Result create() {
@@ -19,5 +20,4 @@ public class ProductCoatingController extends Controller {
     public Result delete(Integer id) {
         return TODO;
     }
-
 }
