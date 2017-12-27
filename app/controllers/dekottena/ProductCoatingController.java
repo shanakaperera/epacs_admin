52e19778e3bd42dce5ce39c8b966d090580bde11
shanakaperera <shanakaperera08@gmail.com>
2017-12-27 20:47:14 +0530

    validation config 1

--- app/controllers/dekottena/ProductCoatingController.java
+++ app/controllers/dekottena/ProductCoatingController.java
@@ -12,30 +12,36 @@ import javax.inject.Inject;
 public class ProductCoatingController extends Controller {
 
     @Inject
-    FormFactory formFactory;
+    private FormFactory formFactory;
 
     public Result home() {
 
         String coating_action = "New Coating";
         String coating_code = "CO552";
         Form<Coating> coatingForm = formFactory.form(Coating.class);
-        Coating preCoating = new Coating(coating_code, coating_action);
-        coatingForm.fill(preCoating);
+        // .fill(new Coating(coating_code, coating_action));
+        return ok(views.html.dekottena.pr_coating_page.pr_coating.render(coatingForm.fill(new Coating(coating_code, coating_action))));
 
-        return ok(views.html.dekottena.pr_coating_page.pr_coating.render(coatingForm));
     }
 
     public Result save() {
 
         Form<Coating> new_coating = formFactory.form(Coating.class).bindFromRequest();
-        Coating coating = new_coating.get();
-        Session s = HibernateUtil.getSessionFactory().openSession();
-        s.save(coating);
-        s.beginTransaction();
-        s.getTransaction().commit();
-        s.close();
 
-        return redirect(routes.ProductCoatingController.home());
+        if (new_coating.hasErrors()) {
+            flash("danger", "Please correct the below form.");
+            return badRequest(views.html.dekottena.pr_coating_page.pr_coating.render(new_coating));
+        } else {
+            Coating coating = new_coating.get();
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.save(coating);
+            s.beginTransaction();
+            s.getTransaction().commit();
+            s.close();
+            flash("success", "Successfully Saved.");
+            return redirect(routes.ProductCoatingController.home());
+        }
+
     }
 
     public Result update(Integer id) {
