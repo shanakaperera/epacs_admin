90f4b47e9fb35db997e3e3d23c0e75254aa4e1c4
sumedha <ssrimal@ymail.com>
2018-01-02 19:03:17 +0530

    making_packages--sss

---
+++ app/controllers/dekottena/cmscontactController.java
@@ -0,0 +1,7 @@
+package controllers.dekottena;
+
+import play.mvc.*;
+
+public class cmscontactController extends Controller{
+
+}
---
+++ app/controllers/dekottena/cmsgalleryController.java
@@ -0,0 +1,11 @@
+package controllers.dekottena;
+
+import play.mvc.*;
+
+public class cmsgalleryController extends Controller {
+
+    public Result home() {
+//        return ok(views.html.dekottena.cms.gallery.gallery.render());
+        return null;
+    }
+}
